85406891f1fe4240c75921646e9be2484915cda8
lyklecharova <lklecharova9@gmail.com>
2025-09-15 22:08:19 +0300

    Add Login and ForgotPass routes to AppRoutes with nested Layout

--- mern-firebase-authentication/firebase-auth-project/src/components/routes/AppRoutes.js
+++ mern-firebase-authentication/firebase-auth-project/src/components/routes/AppRoutes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../layout/Layout';
 import Home from '../../pages/home/Home';
+import Login from '../../pages/login/Login';
+import ForgotPass from '../../pages/login/ForgotPass';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
+                <Route path="login" element={<Login />}></Route>
+                <Route path="forgot" element={<ForgotPass />}></Route>
             </Route>
         </Routes>
     );
