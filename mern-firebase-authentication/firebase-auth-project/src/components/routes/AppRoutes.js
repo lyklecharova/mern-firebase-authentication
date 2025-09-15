fdfbb74138487f6190c77caae672659be9b6bd18
lyklecharova <lklecharova9@gmail.com>
2025-09-15 21:28:21 +0300

    feat: add nested routing with Layout and Home using React Router v6

---
+++ mern-firebase-authentication/firebase-auth-project/src/components/routes/AppRoutes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import Home from '../../pages/home/Home';
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    );
+};
+export default AppRoutes;
