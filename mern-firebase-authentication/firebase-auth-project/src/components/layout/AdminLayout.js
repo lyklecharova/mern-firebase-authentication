8c0b19c9a1a8b719e4d18a59ab37b3cbe57e91bb
lyklecharova <lklecharova9@gmail.com>
2025-09-19 10:38:33 +0300

    feat: add SideBar to AdminLayout for visible navigation

---
+++ mern-firebase-authentication/firebase-auth-project/src/components/layout/AdminLayout.js
@@ -0,0 +1,70 @@
+import { Transition } from '@headlessui/react';
+import React, { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import TopBar from '../header/TopBar';
+
+const AdminLayout = () => {
+    const [showNav, setShowNav] = useState(true);
+    const [isMobile, setIsMobile] = useState(false);
+
+    const handleResize = () => {
+        if (window.innerWidth <= 768) {
+            setIsMobile(true);
+            setShowNav(false);
+        } else {
+            setIsMobile(false);
+            setShowNav(true);
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('resize', handleResize);
+            handleResize(); // Call on initial load
+        }
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <div className="d-flex bg-light">
+            <TopBar showNav={showNav} setShowNav={setShowNav}/>
+            <Transition
+                as="div"
+                show={showNav}
+                enter="transition-transform duration-400"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition-transform duration-400"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+            ></Transition>
+
+            <div
+                className={`flex-grow-1 ${
+                    showNav && !isMobile ? 'ms-240' : ''
+                }`}
+                // style={{transition: 'margin 400ms'}}
+                style={{
+                    transition: 'all 0.6s',
+                    left: showNav ? '280px' : '0',
+                    right: '0',
+                }}
+            >
+                <main
+                    className="p-4 mt-5  bg-light"
+                    style={{
+                        transition: 'all 0.6s',
+                        left: showNav ? '280px' : '0',
+                        right: '0',
+                    }}
+                >
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default AdminLayout;
