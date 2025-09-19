c1d15bf2cc46cb1a18bf954de74cabb1e2a2ff7c
lyklecharova <lklecharova9@gmail.com>
2025-09-19 10:36:08 +0300

    feat: add Register, RegisterComplete pages and AdminLayout routing

--- mern-firebase-authentication/firebase-auth-project/src/components/routes/AppRoutes.js
+++ mern-firebase-authentication/firebase-auth-project/src/components/routes/AppRoutes.js
@@ -4,6 +4,9 @@ import Layout from '../layout/Layout';
 import Home from '../../pages/home/Home';
 import Login from '../../pages/login/Login';
 import ForgotPass from '../../pages/login/ForgotPass';
+import Register from '../../pages/register/Register';
+import RegisterComplete from '../../pages/register/RegisterComplete';
+import AdminLayout from '../layout/AdminLayout';
 
 const AppRoutes = () => {
     return (
@@ -11,8 +14,14 @@ const AppRoutes = () => {
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="login" element={<Login />}></Route>
-                <Route path="register" element={<Login />}></Route>
+                <Route path="register" element={<Register />}></Route>
+                <Route
+                    path="register/complete"
+                    element={<RegisterComplete />}
+                ></Route>
+
                 <Route path="forgot" element={<ForgotPass />}></Route>
+                <Route path="/admin" element={<AdminLayout />}></Route>
             </Route>
         </Routes>
     );
